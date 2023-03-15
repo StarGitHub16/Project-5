bcb2b18972319975a3def47894d28f0887d05b8a
Melad <melad3001@gmail.com>
2023-03-15 01:16:58 -0500

    Added React Router, for single page navigation!

--- univeristy-app/src/App.js
+++ univeristy-app/src/App.js
@@ -8,57 +8,42 @@ function Navbar() {
     <div id="navbar">
       <div id="logo">UniveristyMingle</div>
       <div>Welcome User</div>
-      <NavLink to={'/'} className="categories" >Category</NavLink>
-      <div className="categories">Category</div>
-      <div className="categories">Category</div>
-      <div className="categories">Category</div>
-      <div className="categories">Category</div>
-      <div className="categories">Category</div>
+      <NavLink to={'/'} className="categories">Home</NavLink>
+      <NavLink to={'/'} className="categories">Category</NavLink>
+      <NavLink to={'/'} className="categories">Category</NavLink>
+      <NavLink to={'/'} className="categories">Category</NavLink>
+      <NavLink to={'/'} className="categories">Category</NavLink>
+      <NavLink to={'/'} className="categories">Category</NavLink>
+      
+      
+      
     </div>
     
   )
 }
 
 //The Homepage containing some about info and how to navigate.
-function homePage() {
+function Homepage() {
+  return(
   <div>
     <h1>Welcome to University Mingle!</h1>
     <br />
-    <h2>Here you can ask questions or find existing questions.
-        Related to Unviersity topics that students have experienced in!
-    </h2>
+    <h2>Here you can chat and ask questions with other students!</h2>
   </div>
+  );
 }
 
-function Main() {
-  return(
-    <BrowserRouter>
-      <Routes>
-        
-      </Routes>
-    </BrowserRouter>
-  )
-}
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element= {<Homepage />} />
+      </Routes>
+      </BrowserRouter>
+      </div>
   );
 }
 
