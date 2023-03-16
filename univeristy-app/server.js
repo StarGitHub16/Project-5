512b8e0ee8efa493e41e733b387f0864f17eeed1
Melad <melad3001@gmail.com>
2023-03-16 01:38:52 -0500

    established connection with MySQL!

--- univeristy-app/server.js
+++ univeristy-app/server.js
@@ -8,8 +8,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
-    database: 'universityapp'
+    password: ""
 });
 
 con.connect(function(err) {
