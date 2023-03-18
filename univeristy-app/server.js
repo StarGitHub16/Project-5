8d7927b805cc584dcda37af8f77e2b4e804914ab
Melad <melad3001@gmail.com>
2023-03-18 00:11:40 -0500

    more server testing.

--- univeristy-app/server.js
+++ univeristy-app/server.js
@@ -45,7 +45,7 @@ app.post('/register', (req, res) => {
     );
 });
 
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
 
     const username = req.body.username
     const password = req.body.password
